05497ff46df888140238af9c44a293102cc0d696
kondhanollamanikesh <kondhanollamanikesh@gmail.com>
2026-07-24 12:16:37 +0530

    Time: 239 ms (10.44%), Space: 25.9 MB (64.18%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -0,0 +1,21 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        maxi=0
+        left=0
+        right=0
+        my_dict={}
+        while right<len(fruits):
+            if fruits[right] not in my_dict:
+                my_dict[fruits[right]]=0
+            my_dict[fruits[right]]+=1
+            while len(my_dict)>2:
+                if my_dict[fruits[left]]>0:
+                    my_dict[fruits[left]]-=1
+                if my_dict[fruits[left]]==0:
+                    del my_dict[fruits[left]]
+                left+=1
+            if len(my_dict)<=2:
+                maxi=max(maxi,right-left+1)
+            right+=1
+        return maxi
+
